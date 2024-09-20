81149f39feb94f2400820ec8f7791817ffa174a4
HollyPurvis <hmp21@students.uwf.edu>
2024-09-20 12:40:03 -0500

    added introduciton function

--- TitanicSimulator/main.py
+++ TitanicSimulator/main.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 
-a = [1,6,8]
-# this is a panda series
-series = pd.Series(a)
-print(series)
-
 # reads data from titanic.csv
-titanic_csv = pd.read_csv("files/titanic.csv")
+# index_col removes index column
+titanic_csv = pd.read_csv("files/titanic.csv", index_col=0)
 
 # turns data into a data frame
 titanic_df = pd.DataFrame(titanic_csv)
 print(titanic_df)
+
+# gets total number of passengers
+total_passengers = titanic_df['Freq'].sum()
+print(total_passengers)
